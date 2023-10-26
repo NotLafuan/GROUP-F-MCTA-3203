500af4c57cbe45082ef8857ef54c7f9102179d28
KhAiman97 <149060814+KhAiman97@users.noreply.github.com>
2023-10-26 18:12:59 +0800

    Added USBread

---
+++ Week 3/arduno usb read.py
@@ -0,0 +1,29 @@
+import serial
+import time
+from serial.serialutil import SerialException
+"""
+Define the serial port and baud rate
+(adjust the port as per your Arduino)
+"""
+ser = serial.Serial('COM6', 9600)
+try:
+    while True:
+        angle = input("Enter servo angle (0-180 degrees): ")
+        if angle.lower() == 'q':
+            break
+        angle = int(angle)
+        if 0 <= angle <= 180:
+            # Send the servo’s angle to the Arduino
+            try:
+                data=str(angle)+"A0"
+                ser.write(str(data).encode()) 
+            except SerialException:
+                print ("zakwan gay shit")
+
+        else:
+            print("Angle must be between 0 and 180 degrees.")
+except KeyboardInterrupt:
+    pass # Handle keyboard interrupt
+finally:
+    ser.close() # Close the serial connection
+print("Serial connection closed.")
---
+++ Week 3/usbread.ino
@@ -0,0 +1,29 @@
+#include <Servo.h>
+char incomingByte;
+String data = ""; 
+
+Servo servo;
+
+void setup()
+{
+     Serial.begin(9600);
+     servo.attach(9);
+}
+
+void loop()
+{
+  if(Serial.available()>0){
+    incomingByte = Serial.read();
+    
+    if (incomingByte == 'A') 
+    {
+      servo.write(data.toInt());
+      data = "";
+    }
+    else 
+    {
+      data+=incomingByte;
+    }
+    }
+  }
+ 
